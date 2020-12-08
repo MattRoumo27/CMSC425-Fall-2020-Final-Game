5b4bdef3609608d6eeb3149f7cf12372337b0f76
Elan Haims <ehaims@umd.edu>
2020-12-07 22:45:20 -0500

    Small UI changes

--- CMSC425 Final Game/Assets/Code/Target.cs
+++ CMSC425 Final Game/Assets/Code/Target.cs
@@ -4,9 +4,13 @@ public class Target : MonoBehaviour
 {
     public float health = 50f;
 
+    public GameObject floatingDamage;
+
     public void TakeDamage(float amount)
     {
         health -= amount;
+        GameObject points =  Instantiate(floatingDamage, transform.position + new Vector3(0, .5f, 0), Quaternion.identity) as GameObject;
+        points.transform.GetChild(0).GetComponent<TextMesh>().text = "" + amount;
         if (health <= 0f)
         {
             Die();
