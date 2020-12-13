a19ce62b1ccd192dea8e24a5973566283e8873a4
Zachary Isaac Mazer <zimazer@gmail.com>
2020-12-13 17:14:03 -0500

    zombie changes plus waves

---
+++ CMSC425 Final Game/Assets/Code/Enemy/Attack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        if (!player)
+        {
+            Debug.Log("DIDNT GET PLAYER");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerStats>().TakeDamage(10);
+        }
+    }
+}
